27fb94f8014609d5241ab27595b095fa97afbc75
Chaimongkol <cpanawasri@gmail.com>
2024-03-23 11:40:53 +0700

    reward cal

--- src/routers/index.js
+++ src/routers/index.js
@@ -10,6 +10,10 @@ const rootRoute = async (app) => {
   app.get("/questions", GetQuestion);
   app.post("/submit-answer", SubmitAnswer);
   app.get("/uploadS3", UploadAnswerS3);
+  app.get('/download/:id', function(req, res){
+    const file = `${__dirname}/public/Reward/Reward${req.params.id ?? 1}.webp`;
+    res.download(file);
+  });
 };
 async function router(fastify) {
     fastify.register(rootRoute);
