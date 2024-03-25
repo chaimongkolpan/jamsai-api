ee2b67ecb0ada292bf4afb98c39e380eb08827ab
Chaimongkol <cpanawasri@gmail.com>
2024-03-25 23:02:03 +0700

    fix submit answer

--- functions/src/services/index.js
+++ functions/src/services/index.js
@@ -476,16 +476,17 @@ const GetQuestion = async (req) => {
 const SubmitAnswer = async (req) => {
   try {
     const { jamsai_id, answers } = req.body;
+    const id = jamsai_id && (typeof jamsai_id == 'string') ? parseInt(jamsai_id) : jamsai_id
     const result = CalculateBenefit(answers);
     const data = {
-        jamsai_id: jamsai_id,
+        jamsai_id: id,
         answers: JSON.stringify(answers),
         created_at: new Date(),
     }
-    if (jamsai_id) {
+    if (id) {
         const existAnswer = await prisma.book_fair_event_answers.findMany({
             where: {
-                jamsai_id: jamsai_id
+                jamsai_id: id
             },
         });
         await prisma.book_fair_event_answers.create({
@@ -525,45 +526,45 @@ const SubmitAnswer = async (req) => {
   }
 };
 const CalculateBenefit = (answers) => {
-    if (answers && answers.length > 2) {
-        if (answers[0] == 1) {
-            if (answers[2] == 1) {
+    if (answers && answers.length > 3) {
+        if (answers[0] == 1 || answers[0] == '1') {
+            if (answers[2] == 1 || answers[0] == '1') {
                 return 12;
-            } else if (answers[2] == 2) {
+            } else if (answers[2] == 2 || answers[0] == '2') {
                 return RandomData([10, 9]);
-            } else if (answers[2] == 3) {
+            } else if (answers[2] == 3 || answers[0] == '3') {
                 return 13;
-            } else if (answers[2] == 4) {
+            } else if (answers[2] == 4 || answers[0] == '4') {
                 return 11;
             } else return 12;
-        } else if (answers[0] == 4) {
-            if (answers[2] == 1) {
+        } else if (answers[0] == 4 || answers[0] == '4') {
+            if (answers[2] == 1 || answers[0] == '1') {
                 return 6;
-            } else if (answers[2] == 2) {
+            } else if (answers[2] == 2 || answers[0] == '2') {
                 return 7;
-            } else if (answers[2] == 3) {
+            } else if (answers[2] == 3 || answers[0] == '3') {
                 return 8;
-            } else if (answers[2] == 4) {
+            } else if (answers[2] == 4 || answers[0] == '4') {
                 return RandomData([6, 7]);
             } else return 6;
-        } else if (answers[0] == 3) {
-            if (answers[2] == 1) {
+        } else if (answers[0] == 3 || answers[0] == '3') {
+            if (answers[2] == 1 || answers[0] == '1') {
                 return 14;
-            } else if (answers[2] == 2) {
+            } else if (answers[2] == 2 || answers[0] == '2') {
                 return 15;
-            } else if (answers[2] == 3) {
+            } else if (answers[2] == 3 || answers[0] == '3') {
                 return 15;
-            } else if (answers[2] == 4) {
+            } else if (answers[2] == 4 || answers[0] == '4') {
                 return 14;
             } else return 14;
-        } else if (answers[0] == 2) {
-            if (answers[2] == 1) {
+        } else if (answers[0] == 2 || answers[0] == '2') {
+            if (answers[2] == 1 || answers[0] == '1') {
                 return RandomData([3, 5]);
-            } else if (answers[2] == 2) {
+            } else if (answers[2] == 2 || answers[0] == '2') {
                 return 4;
-            } else if (answers[2] == 3) {
+            } else if (answers[2] == 3 || answers[0] == '3') {
                 return 2;
-            } else if (answers[2] == 4) {
+            } else if (answers[2] == 4 || answers[0] == '4') {
                 return 1;
             } else return 4;
         } else return 1;
