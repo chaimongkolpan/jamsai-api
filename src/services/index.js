cf29fd0403e88429169190e11cb8132a3861ce2c
Chaimongkol <cpanawasri@gmail.com>
2024-03-26 16:34:52 +0700

    s3 key

--- src/services/index.js
+++ src/services/index.js
@@ -17,12 +17,14 @@ const PATH = process.env.AWS_S3_PATH;
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+    region: "ap-southeast-1"
 });
 const BUCKET_ANSWER = process.env.AWS_S3_ANSWER_BUCKET;
 const PATH_ANSWER = process.env.AWS_S3_ANSWER_PATH;
 const s3_ANSWER = new AWS.S3({
     accessKeyId: process.env.AWS_S3_ANSWER_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_S3_ANSWER_SECRET_ACCESS_KEY,
+    region: "ap-southeast-1"
 });
 
 const Login = async (req) => {
